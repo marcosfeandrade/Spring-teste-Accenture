7df14ec8da9848c35a8d54d5c385a90bac1c509e
marcosfandrade <marcos.fellipe2211@gmail.com>
2021-11-06 19:24:40 -0300

    Upload contato

---
+++ contatos/src/test/java/com/acc/contatos/ContatosApplicationTests.java
@@ -0,0 +1,14 @@
+package com.acc.contatos;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
