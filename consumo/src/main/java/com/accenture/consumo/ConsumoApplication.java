0363b13db9058d46d16debbd8a587282913efaa2
marcosfandrade <marcos.fellipe2211@gmail.com>
2021-11-06 20:08:50 -0300

    Upload consumo

---
+++ consumo/src/main/java/com/accenture/consumo/ConsumoApplication.java
@@ -0,0 +1,16 @@
+package com.accenture.consumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApplication.class, args);
+	}
+
+}
+// localhost:8080/01523040
